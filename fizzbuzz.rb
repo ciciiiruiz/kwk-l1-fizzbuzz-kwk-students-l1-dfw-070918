6728780c5a3ffa986dbe6de22bfd3b6435a895b9
ciciiiruiz <ciciiiruiz@gmail.com>
2018-07-10 21:34:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,8 @@
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0
+    puts "Fizz"
+    elsif int % 5 == 0
+    puts "Buzz"
+  else int % 3 == 0 && 5 == 0
   end
 end
